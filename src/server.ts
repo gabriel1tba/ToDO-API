27ae40e6cd24640b5632308864211c36e099a0ec
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-04-16 15:22:53 -0300

    feat: configurando prettier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
-import routes from "./routes";
+import routes from './routes';
 
 const app = express();
 
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Server inicializado! 🔥");
+  console.log('Server inicializado! 🔥');
 });
