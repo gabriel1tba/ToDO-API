4168c91aad1cab728d2e85035cf1a6c6e7a94fa9
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-04-17 09:39:59 -0300

    feat: atualizando model de todos

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,21 +1,32 @@
-import v4 from '../utils/uuidv4';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 interface ITodos {
   id: string;
   title: string;
   description: string;
 }
-
+@Entity('todos')
 class Todo implements ITodos {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   title: string;
+
+  @Column()
   description: string;
 
-  constructor({ title, description }: Omit<ITodos, 'id'>) {
-    this.id = v4();
-    this.title = title;
-    this.description = description;
-  }
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Todo;
