6ed46d32931c9d458fe6041d9f41066ae3a8be7f
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-12-10 09:43:07 -0300

    feat: criando TodoController

---
+++ src/controllers/TodoController.ts
@@ -0,0 +1,84 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import TodosRepository from '../repositories/TodosRepository';
+
+import CreateTodoService from '../services/CreateTodoService';
+import UpdateTodoService from '../services/UpdateTodoService';
+import DeleteTodoService from '../services/DeleteTodoService';
+
+import Errors from '../erros/Errors';
+
+class TodoController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    try {
+      const { user_id } = request.params;
+
+      const todosRepository = getCustomRepository(TodosRepository);
+
+      const todos = await todosRepository.find({ where: { user_id } });
+
+      return response.json(todos);
+    } catch (err) {
+      const error = err as Errors;
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  public async store(request: Request, response: Response): Promise<Response> {
+    try {
+      const { user_id, completed, title, description } = request.body;
+
+      const CreateTodo = new CreateTodoService();
+
+      const todo = await CreateTodo.execute({
+        user_id,
+        completed,
+        title,
+        description,
+      });
+
+      return response.json(todo);
+    } catch (err) {
+      const error = err as Errors;
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    try {
+      const { id, completed, title, description } = request.body;
+
+      const UpdateTodo = new UpdateTodoService();
+
+      const todo = await UpdateTodo.execute({
+        id,
+        completed,
+        title,
+        description,
+      });
+
+      return response.json(todo);
+    } catch (err) {
+      const error = err as Errors;
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    try {
+      const { id } = request.body;
+
+      const DeleteTodo = new DeleteTodoService();
+
+      const deletedTodo = await DeleteTodo.execute({ id });
+
+      return response.json(deletedTodo);
+    } catch (err) {
+      const error = err as Errors;
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export default TodoController;
--- src/routes/todos.routes.ts
+++ src/routes/todos.routes.ts
@@ -1,87 +1,17 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 
 import routeAuthenticated from '../middlewares/routeAuthenticated';
-
-import TodosRepository from '../repositories/TodosRepository';
-import CreateTodoService from '../services/CreateTodoService';
-import UpdateTodoService from '../services/UpdateTodoService';
-import DeleteTodoService from '../services/DeleteTodoService';
-
-import Errors from '../erros/Errors';
+import TodoController from '../controllers/TodoController';
 
 const todosRouter = Router();
 
+const todoController = new TodoController();
+
 todosRouter.use(routeAuthenticated);
 
-todosRouter.post('/', async (request, response) => {
-  try {
-    const { user_id, completed, title, description } = request.body;
-
-    const CreateTodo = new CreateTodoService();
-
-    const todo = await CreateTodo.execute({
-      user_id,
-      completed,
-      title,
-      description,
-    });
-
-    return response.json(todo);
-  } catch (err) {
-    const error = err as Errors;
-    return response.status(400).json({ error: error.message });
-  }
-});
-
-todosRouter.get('/:user_id', async (request, response) => {
-  try {
-    const { user_id } = request.params;
-
-    const todosRepository = getCustomRepository(TodosRepository);
-
-    const todos = await todosRepository.find({ where: { user_id } });
-
-    return response.json(todos);
-  } catch (err) {
-    const error = err as Errors;
-    return response.status(400).json({ error: error.message });
-  }
-});
-
-todosRouter.patch('/', async (request, response) => {
-  try {
-    const { id, completed, title, description } = request.body;
-
-    const UpdateTodo = new UpdateTodoService();
-
-    const todo = await UpdateTodo.execute({
-      id,
-      completed,
-      title,
-      description,
-    });
-
-    return response.json(todo);
-  } catch (err) {
-    const error = err as Errors;
-    return response.status(400).json({ error: error.message });
-  }
-});
-
-todosRouter.delete('/', async (request, response) => {
-  try {
-    const { id } = request.body;
-
-    const DeleteTodo = new DeleteTodoService();
-
-    const deletedTodo = await DeleteTodo.execute({ id });
-
-    return response.json(deletedTodo);
-  } catch (err) {
-    const error = err as Errors;
-    return response.status(400).json({ error: error.message });
-  }
-});
+todosRouter.get('/:user_id', todoController.index);
+todosRouter.post('/', todoController.store);
+todosRouter.patch('/', todoController.update);
+todosRouter.delete('/', todoController.delete);
 
 export default todosRouter;
