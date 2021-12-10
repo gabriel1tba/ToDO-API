a45910a12fae34a8180b4e357172ed181adacd20
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-12-10 10:08:18 -0300

    feat: criando UserController

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+import Errors from '../erros/Errors';
+
+class UserController {
+  public async store(request: Request, response: Response): Promise<Response> {
+    try {
+      const { name, email, password, confirmPassword } = request.body;
+
+      const createUser = new CreateUserService();
+
+      const user = await createUser.execute({
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      // @ts-expect-error
+      delete user.password;
+      // @ts-expect-error
+      delete user.confirmPassword;
+
+      return response.json(user);
+    } catch (err) {
+      const error = err as Errors;
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export default UserController;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,33 +1,10 @@
 import { Router } from 'express';
-import Errors from '../erros/Errors';
+
+import UserController from '../controllers/UserController';
 
 const usersRouter = Router();
+const userController = new UserController();
 
-import CreateUserService from '../services/CreateUserService';
-
-usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password, confirmPassword } = request.body;
-
-    const createUser = new CreateUserService();
-
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-      confirmPassword,
-    });
-
-    // @ts-expect-error
-    delete user.password;
-    // @ts-expect-error
-    delete user.confirmPassword;
-
-    return response.json(user);
-  } catch (err) {
-    const error = err as Errors;
-    return response.status(400).json({ error: error.message });
-  }
-});
+usersRouter.post('/', userController.store);
 
 export default usersRouter;
